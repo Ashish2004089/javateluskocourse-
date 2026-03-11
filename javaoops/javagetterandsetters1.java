96d86e1f30f0c2eb71eac9e202cd0315c7b59445
Ashish2004089 <131390150+Ashish2004089@users.noreply.github.com>
2026-03-11 08:39:11 +0530

    Update javagetterandsetters1.java

--- javaoops/javagetterandsetters1.java
+++ javaoops/javagetterandsetters1.java
@@ -1,1 +1,36 @@
 
+package oopspractice;
+
+class Student{
+	
+	//instance variable ->they will only get created when the object of the class is created 
+	int age;
+	
+	
+	
+	//they are called as the methods of the class
+	
+	//setter 
+	void sleep(int time) {
+		age = time;
+	}
+
+	//getter 
+	int getsleep() {
+		return age;
+	}
+}
+public class oopscode1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student stu = new Student();
+		
+		stu.sleep(9);
+		
+		System.out.println(stu.getsleep());
+		
+	}
+
+}
