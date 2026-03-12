f154b26ab8ace752b92c94d419021769cb16f135
Ashish2004089 <131390150+Ashish2004089@users.noreply.github.com>
2026-03-12 15:02:22 +0530

    Create inheritance5.java

---
+++ javaoops/inheritance5.java
@@ -0,0 +1,49 @@
+package oopspractice;
+
+class demo1{
+	
+	int a,b;
+	demo1(){
+		System.out.println("demo1 zero param constructor");
+	}
+	demo1(int a,int b){
+		this.a = a;
+		this.b = b;
+		System.out.println("demo1 1 param constructor");
+	}
+	
+}
+
+class demo2 extends demo1{
+	int x,y;
+	
+	demo2(){
+		super();
+		System.out.println("demo2 zero param constructor");
+	}
+	demo2(int x,int y){
+		//yaha pr super() written hai behind the scene vo parent class 0 param constructor ko call karega then yaha pr control 
+		//aayega 
+		
+		this.x = x;
+		this.y = y;
+		System.out.println("demo 2 param constructor");
+		
+	}
+	void disp() {
+		System.out.println("x "+x +" y "+y);
+		System.out.println("a "+a+ " b "+b);
+	}
+	
+}
+public class inheritance5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//this -> it is used to call the constructor of the current  class
+		 demo2 d2 = new demo2(4,5);
+		 d2.disp();
+		 
+	}
+
+}
