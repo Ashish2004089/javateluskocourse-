6c29c00fcefbee0bb281bb708264831c13213fb9
Ashish2004089 <131390150+Ashish2004089@users.noreply.github.com>
2026-03-21 13:19:40 +0530

    Create string6.java

---
+++ javaarraysandstring/string6.java
@@ -0,0 +1,14 @@
+package arraystringpractice;
+
+public class String6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer str1 = new StringBuffer("ashish");
+		StringBuffer str2 = new StringBuffer("ashish");
+		System.out.println(str1==str2);
+		System.out.println(str1.equals(str2));
+
+	}
+
+}
