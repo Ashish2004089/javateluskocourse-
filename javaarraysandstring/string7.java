597f210939d0f141153f0992ca1bfb576ca3a7e8
Ashish2004089 <131390150+Ashish2004089@users.noreply.github.com>
2026-03-21 13:19:56 +0530

    Create string7.java

---
+++ javaarraysandstring/string7.java
@@ -0,0 +1,17 @@
+package arraystringpractice;
+
+public class String7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String temp = "rajaRamMohanRoy";
+		System.out.println(temp.endsWith("Roy"));
+		System.out.println(temp.charAt(1));
+		System.out.println(temp.startsWith("raja"));
+		System.out.println(temp.length());
+		System.out.println(temp.toUpperCase());
+		System.out.println(temp.toLowerCase());
+
+	}
+
+}
